b050ae96fc61332595f1ad4b0910b1ceadab5377
AselK <asel.nurmuhanbetova@gmail.com>
2018-01-21 17:30:05 +0300

    adding FinickyCounter

---
+++ chapter_02/03_FinickyCounter_break_continue/FinickyCounter_break_continue.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int count = 0;
+	while (true)
+	{
+		count += 1;
+		//заканчиваем цикл, если значение count превышает 10
+		if (count > 10)
+		{
+			break;
+		}
+		//пропускаем число 5
+		if (count == 5)
+		{
+			continue;
+		}
+		cout << count << endl;
+	}
+}
