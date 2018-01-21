e334fb33e404ce10c8da104ec295baac1e28b8d5
AselK <asel.nurmuhanbetova@gmail.com>
2018-01-21 13:56:48 +0300

    Loading chapter 1 files

---
+++ chapter_01/exercise_c01_01/chapter_c01_01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Input Number: " << endl;
+	cin >> a;
+	int b;
+	cout << "Input Number: " << endl;
+	cin >> b;
+	int c;
+	cout << "Input Number: " << endl;
+	cin >> c;
+	int d;
+	
+	d = a + b + c;
+	
+	//cout << d << endl;
+	
+	int k;
+	k = d / 3;
+	
+	cout << k << endl; 
+}
