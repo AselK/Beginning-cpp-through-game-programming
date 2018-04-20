f60bed9ac188ae1b3394224ced0d32bf59e1e6dc
AselK <asel.nurmuhanbetova@gmail.com>
2018-04-20 20:22:56 +0300

    adding MenuChooser_switch

---
+++ chapter_02/07_MenuChooser_switch/MenuChooser_switch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	std::cout <<"Difficulty Levels:" <<std::endl;
+	std::cout <<"1 - Easy" <<std::endl;
+	std::cout <<"2 - Normal" <<std::endl;
+	std::cout <<"3 - Hard" <<std::endl;
+	int choice;
+	std::cout <<"Choice: ";
+	std::cin >> choice;
+	
+	switch (choice)
+	{
+		case 1:
+			std::cout <<"You picked Easy.\n";
+			break;
+		case 2:
+			std::cout <<"You picked Normal.\n";
+			break;
+		case 3:
+			std::cout <<"You picked Hard.\n";
+			break;
+		default:
+			std::cout <<"You made an illegal choice.\n";
+			
+	}
+}
